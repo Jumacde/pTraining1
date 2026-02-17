b973f6d803bacebc25714fd7fdddddac546b4ab6
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2026-02-17 16:58:33 +0100

    commit

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print ("test")
+class member:
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+    
+    def prof(self):
+        print(f"i am {self.name} and {self.age} years old")
+
+def main():
+    p1 = member("jene", 17, "female")
+    p1.prof()
+
+if __name__ == "__main__":
+    main()
