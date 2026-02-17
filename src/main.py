d18d720944ef2a3651aea81fcbd7f1e5d7ddc1a7
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2026-02-17 18:11:28 +0100

    commited commments

--- src/main.py
+++ src/main.py
@@ -3,8 +3,10 @@ class Member:
         self.name = name
         self.age = age
         self.sex = sex
-        self.setPronomen()
+        # initialize the function to use the results of them to other functions 
+        self.setPronomen() 
 
+    # function: check Sex and show it.
     def checkSex(self):
         if self.sex == "male":
             return"man"
@@ -13,6 +15,7 @@ class Member:
         else:
             return "unknown"
         
+    # function: set the personal pronomen via the function checkSex()
     def setPronomen(self):
         if self.checkSex() == "man":
             self.pronomen = "he"
@@ -21,15 +24,18 @@ class Member:
         else:
             self.pronomen = "they"
 
+    # function: show pfofile of 3 persons(name, age and sex).
     def prof(self):
         print(f"{self.name} is {self.age} years old and a {self.checkSex():}")
     
+    # function: check age and show whether the parson can drink alcohole or not.
     def drinkAge(self):
         if self.age >= 20:
             print(f"{self.pronomen} can drink alcohole")
         else:
             print(f"{self.pronomen} cannot drink alcohole")
 
+# set the profile-date of 3 persoons.
 def main():
     p1 = Member("Alice", 17, "female")
     p1.setPronomen()
@@ -38,6 +44,7 @@ def main():
     p3 = Member("Num1", 33, "none")
     p3.setPronomen()
     
+    # to execute all functions for 3 persons.
     for p in [p1, p2, p3]:
         p.setPronomen()
         p.prof()
