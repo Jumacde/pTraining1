6cf36d3555e75d6d93dc8b803a4179fd77c5d157
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2026-02-17 17:09:04 +0100

    commited

--- src/main.py
+++ src/main.py
@@ -1,15 +1,28 @@
-class member:
+class Member:
     def __init__(self, name, age, sex):
         self.name = name
         self.age = age
         self.sex = sex
     
     def prof(self):
-        print(f"i am {self.name} and {self.age} years old")
+        print(f"{self.name} is {self.age} years old")
+    
+    def drinkAge(self):
+        adult = Member(self.name, self.age, self.sex)
+
+        if adult.age >= 20:
+            print(f"{self.name} can drink alcohole")
+        else:
+            print(f"{self.name} cannot drink alcohole")
 
 def main():
-    p1 = member("jene", 17, "female")
+    p1 = Member("Alice", 17, "female")
+    p2 = Member("Bob", 45, "male")
+    
     p1.prof()
+    p1.drinkAge()
+    p2.prof()
+    p2.drinkAge()
 
 if __name__ == "__main__":
     main()
